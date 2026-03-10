05c020e87500b8898ba93feab057f54387b81f96
Doroshchenkov <doroshchenkov.d_ipz23@rcit.ukr.education>
2026-03-10 21:43:24 +0200

    Fix Render config

--- Program.cs
+++ Program.cs
@@ -36,8 +36,14 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Projects}/{action=Index}/{id?}");
 
 app.MapRazorPages();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
 
 app.Run();
